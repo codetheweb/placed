6b705070b485a4dbc507e1e1820b7c35a0a05929
Max Isom <hi@maxisom.me>
2022-11-18 20:22:17 -0800

    Faster gzip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use chrono::NaiveDateTime;
-use flate2::write::ZlibEncoder;
-use flate2::Compression;
+use gzp::deflate::Gzip;
+use gzp::par::compress::ParCompress;
+use gzp::par::compress::ParCompressBuilder;
+use gzp::ZWriter;
 use serde::Serialize;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::io::BufRead;
-use std::io::{BufReader, BufWriter};
 
 #[macro_use]
 extern crate serde_derive;
@@ -29,22 +29,21 @@ fn main() {
 
     let file = fs::File::open(filename).expect("Could not open file");
     let out = fs::File::create(out_filename).expect("Could not create file");
-    let out_buffered = BufWriter::new(out);
-    let out_compressed_writer = ZlibEncoder::new(out_buffered, Compression::default());
-    let mut out_serializer = Serializer::new(out_compressed_writer);
 
-    let reader = BufReader::new(file);
+    let mut out_compressed_writer: ParCompress<Gzip> = ParCompressBuilder::new().from_writer(out);
+    let mut out_serializer = Serializer::new(&mut out_compressed_writer);
+
+    let mut reader = csv::Reader::from_reader(file);
 
     let mut first_timestamp = None;
 
     let mut color_map: HashMap<String, u16> = HashMap::new();
 
-    for line in reader.lines().skip(1) {
-        let line = line.unwrap();
-        let columns = line.split(',').collect::<Vec<_>>();
+    for result in reader.records() {
+        let record = result.expect("Could not read record");
 
         let timestamp =
-            NaiveDateTime::parse_from_str(columns.first().unwrap(), "%Y-%m-%d %H:%M:%S%.3f UTC")
+            NaiveDateTime::parse_from_str(record.get(0).unwrap(), "%Y-%m-%d %H:%M:%S%.3f UTC")
                 .expect("Could not parse timestamp");
 
         first_timestamp = match first_timestamp {
@@ -52,13 +51,15 @@ fn main() {
             None => Some(timestamp),
         };
 
-        let color_str = columns.get(2).unwrap().to_string();
+        let color_str = record.get(2).unwrap().to_string();
         if !color_map.contains_key(&color_str) {
             color_map.insert(color_str.clone(), color_map.len() as u16);
         }
 
-        let x_str = columns.get(3).unwrap().replace('"', "");
-        let y_str = columns.get(4).unwrap().replace('"', "");
+        let clean_coords = record.get(3).unwrap().replace('"', "");
+        let mut coords = clean_coords.split(',');
+        let x_str = coords.next().unwrap();
+        let y_str = coords.next().unwrap();
         let x = x_str.parse::<u16>().expect("Could not parse x coordinate");
         let y = y_str.parse::<u16>().expect("Could not parse y coordinate");
 
@@ -73,4 +74,6 @@ fn main() {
 
         pixel.serialize(&mut out_serializer).unwrap();
     }
+
+    out_compressed_writer.finish().unwrap();
 }
